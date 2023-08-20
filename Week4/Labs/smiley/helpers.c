7a1189ca555490047824c524df9d4b553678dad0
Filip Kocian <filkocian@gmail.com>
2023-08-20 15:11:56 +0200

    week 4 labs

---
+++ Week4/Labs/smiley/helpers.c
@@ -0,0 +1,18 @@
+#include "helpers.h"
+#include "stdio.h"
+
+void colorize(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++){
+
+        for (int j = 0; j < width; j++){
+            if (image[i][j].rgbtBlue == 0
+                && image[i][j].rgbtGreen == 0
+                && image[i][j].rgbtRed == 0) {
+                    image[i][j].rgbtBlue = 175;
+                    image[i][j].rgbtGreen = 219;
+                    image[i][j].rgbtRed = 44;
+            }
+        }
+    }
+}
